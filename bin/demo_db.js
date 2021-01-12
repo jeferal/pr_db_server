0bccf6d1c26b615ff96b4a43d2b28f20fee8a0e9
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2021-01-12 15:24:13 +0100

    created server for mysql

--- bin/demo_db.js
+++ bin/demo_db.js
@@ -12,21 +12,19 @@ var con = mysql.createConnection({
 
 
 app.get('/', (req, res)=> {
-    res.send('Hello World');
+    var sql_command = "SELECT * FROM trajectories";
+    con.query(sql_command, function(err, result) {
+        if (err) throw err;
+        console.log("Result: ", result);
+        res.send(result);
+    });
 });
 
 app.listen(8000, () => {
     console.log('Example app listening on port 8000!');
 });
 
-
 con.connect(function(err){
     if (err) throw err;
     console.log("Connected to db!");
-    var sql_command = "SELECT * FROM trajectories";
-    con.query(sql_command, function(err, result) {
-        if (err) throw err;
-        console.log("Result: ", result);
-    });
-    
 });

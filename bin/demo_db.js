08d12ee7c066e13789e2824b123c60df201927a2
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2021-01-13 13:08:05 +0100

    same as ros computer

--- bin/demo_db.js
+++ bin/demo_db.js
@@ -1,25 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
-<<<<<<< HEAD
-app.use(express.static('.'));
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-
-=======
->>>>>>> 0d4a8df82a8bce11884acf3d8610a7128fb87b26
 app.listen(8000);
 console.log('Example app listening on port 8000!');
 
 
-<<<<<<< HEAD
-/*
-var mysql = require('mysql');
-=======
 
 const mysql = require('mysql');
->>>>>>> 0d4a8df82a8bce11884acf3d8610a7128fb87b26
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -33,28 +20,10 @@ con.connect(function(err){
     if (err) throw err;
     console.log("Connected to db!");
 });
-*/
+
 
 
 app.get('/references', (req, res)=> {
-    /*
-    var sql_command = "SELECT * FROM trajectories";
-    con.query(sql_command, function(err, result) {
-        if (err) throw err;
-        console.log("Result: ", result);
-        //res.send(result);
-        res.send(result);
-    });
-    */
-   res.send({ "name":"John", "age":30, "car":null });
-});
-
-<<<<<<< HEAD
-app.post('/references', (req, res)=> {
-    /*
-=======
-app.get('/references', (req, res)=> {
->>>>>>> 0d4a8df82a8bce11884acf3d8610a7128fb87b26
     var sql_command = "SELECT * FROM trajectories";
     con.query(sql_command, function(err, result) {
         if (err) throw err;
@@ -62,6 +31,4 @@ app.get('/references', (req, res)=> {
         res.header("Access-Control-Allow-Origin", "*");
         res.json(result);
     });
-    */
-   res.send({ "name":"Jesús", "age":30, "car":null });
 });
